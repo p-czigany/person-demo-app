7032727e7156ac25dde181f67dfa9c04e5ae2670
Peter Czigany <p.czigany@gmail.com>
2023-11-15 22:29:37 +0100

    refactor test

--- src/test/java/com/peterczigany/person/PersonContactTest.java
+++ src/test/java/com/peterczigany/person/PersonContactTest.java
@@ -4,11 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
-public class PersonContactTest {
+class PersonContactTest {
 
   @Test
   void create() {
-    PersonContact contact = new PersonContact(null, PersonContact.ContactType.EMAIL, "holly@black.com");
+    PersonContact contact =
+        new PersonContact(null, PersonContact.ContactType.EMAIL, "holly@black.com");
 
     assertThat(contact.getContactType()).isEqualTo(PersonContact.ContactType.EMAIL);
     assertThat(contact.getContactValue()).isEqualTo("holly@black.com");
