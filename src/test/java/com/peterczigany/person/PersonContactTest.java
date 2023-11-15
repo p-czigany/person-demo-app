329f4a511fb811c099407adad3b2b5a3af848052
Peter Czigany <p.czigany@gmail.com>
2023-11-15 22:20:49 +0100

    test successful contact pojo creation

---
+++ src/test/java/com/peterczigany/person/PersonContactTest.java
@@ -0,0 +1,16 @@
+package com.peterczigany.person;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class PersonContactTest {
+
+  @Test
+  void create() {
+    PersonContact contact = new Contact(null, PersonContact.ContactType.EMAIL, "holly@black.com");
+
+    assertThat(contact.getContactType()).isEqualTo(PersonContact.ContactType.EMAIL);
+    assertThat(contact.getContactValue()).isEqualTo("holly@black.com");
+  }
+}
