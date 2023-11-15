465c9afcd532773e5f98d9f3288c7d317d71cab2
Peter Czigany <p.czigany@gmail.com>
2023-11-15 22:09:58 +0100

    add integration test config for SQL Server running in container

--- src/integration-test/java/PersonApplicationAcceptanceTest.java
+++ src/integration-test/java/PersonApplicationAcceptanceTest.java
@@ -10,12 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.MSSQLServerContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(classes = PersonApplication.class)
+@ActiveProfiles("integrationtest")
 @AutoConfigureMockMvc
 @Testcontainers
 class PersonApplicationAcceptanceTest {
