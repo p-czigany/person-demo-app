15e973aa4098c7de6c3625230939d1ea536cffaf
Peter Czigany <p.czigany@gmail.com>
2023-11-15 22:29:06 +0100

    implement contact pojo

---
+++ src/main/java/com/peterczigany/person/PersonContact.java
@@ -0,0 +1,44 @@
+package com.peterczigany.person;
+
+public class PersonContact {
+  private Long id;
+  private ContactType contactType;
+  private String contactValue;
+
+  public enum ContactType {
+    PHONE,
+    EMAIL
+  }
+
+  public PersonContact() {}
+
+  public PersonContact(Long id, ContactType contactType, String contactValue) {
+    this.id = id;
+    this.contactType = contactType;
+    this.contactValue = contactValue;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public ContactType getContactType() {
+    return contactType;
+  }
+
+  public void setContactType(ContactType contactType) {
+    this.contactType = contactType;
+  }
+
+  public String getContactValue() {
+    return contactValue;
+  }
+
+  public void setContactValue(String contactValue) {
+    this.contactValue = contactValue;
+  }
+}
