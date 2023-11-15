4b3d4c3ed5a6070c0ff6c9e78205dbfe12516f8d
Peter Czigany <p.czigany@gmail.com>
2023-11-16 00:01:18 +0100

    fix test

--- src/test/java/com/peterczigany/person/PersonRepositoryTest.java
+++ src/test/java/com/peterczigany/person/PersonRepositoryTest.java
@@ -5,7 +5,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.peterczigany.person.model.Person;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+@DataJpaTest
 public class PersonRepositoryTest {
 
   @Autowired PersonRepository repository;
