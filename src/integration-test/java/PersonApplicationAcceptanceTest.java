9b2218895f84b96e51e3f5c2172e6981ed557df6
Peter Czigany <p.czigany@gmail.com>
2023-11-15 21:38:41 +0100

    test that mockMvc is working

--- src/integration-test/java/PersonApplicationAcceptanceTest.java
+++ src/integration-test/java/PersonApplicationAcceptanceTest.java
@@ -1,8 +1,12 @@
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.peterczigany.person.PersonApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(classes = PersonApplication.class)
@@ -13,4 +17,11 @@ class PersonApplicationAcceptanceTest {
   @Test
   @SuppressWarnings("java:S2699")
   void emptyTest() {}
+
+  @Test
+  void testMockMvcWorking() throws Exception {
+    mockMvc
+        .perform(get("/some/nonExistent/endpoint").contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isNotFound());
+  }
 }
